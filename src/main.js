fa782077f94773a4212f78162229d03ed3510126
ZZzzaaKK <66885975+ZZzzaaKK@users.noreply.github.com>
2022-09-05 15:25:01 +0200

    Add button to upload an image from file dialog

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const { open } = window.__TAURI__.dialog;
+
 const { invoke } = window.__TAURI__.tauri;
 
 let greetInputEl;
@@ -12,4 +14,24 @@ async function greet() {
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
 }
 
+async function openFileSystem() {
+  greetMsgEl.textContent = "Hello";
+  // Open a selection dialog for image files
+  const selected = await open({
+    multiple: true,
+    filters: [{
+      name: 'Image',
+      extensions: ['png', 'jpeg']
+    }]
+  });
+  if (Array.isArray(selected)) {
+    // user selected multiple files
+  } else if (selected === null) {
+    // user cancelled the selection
+  } else {
+    // user selected a single file
+  }
+}
+
 window.greet = greet;
+window.openFileSystem = openFileSystem;
